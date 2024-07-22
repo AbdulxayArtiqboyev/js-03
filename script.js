dfbfee19f642f8bb9d120ad05f5a10fa662c4bf5
AbdulxayArtiqboyev <ggithub164@gmail.com>
2024-07-22 17:15:16 +0500

    js-03

---
+++ script.js
@@ -0,0 +1,240 @@
+// {
+//     // masala 1
+//     let array = [14,11,12,55,43,46,67];
+// for(let i = 0; i < array.length;i++){
+//     if(array[i] % 2 == 0){
+//         console.log(array[i]);
+//     }
+// }
+// }
+
+// {
+//     // masala 2
+//     for (let index = 0; index <= 50; index++) {
+//         if(index % 2 !==0){
+//             console.log(index);
+//         }
+        
+//     }
+// }
+
+// {
+//     // masala 3
+//     let number = +prompt('son kiriting');
+//     let sum = 0;
+//     for(let i = 1; i <= number; i++){
+//         sum += i
+//     }
+//     console.log(`yigindisi: ${sum}`);
+    
+
+// }
+
+// {
+//     // masala 4
+//     let odd = 0;
+//     let even = 0;
+//     for(let i = 1; i <= 10;i++){
+//         if(i % 2 !== 0){
+//            odd += i
+//         }
+//         else if( i % 2 == 0){
+//             even += i
+//         }
+        
+//     }
+//     console.log(odd / even + ' : juft sonlar toqdan shuncha katta');
+    
+    
+// }
+
+// {
+//     // masala 5
+//     let n = +prompt('n son kiriting');
+//     for(let i = 1; i <= n; i++){
+//         if(i % 2 == 0 && i % 3 == 0){
+//             console.log(`2 ga va 3 bolinadigan sonlar: ${i}`);
+//         }
+//     }
+// }
+
+// {
+//     // masala 6
+//     let a = +prompt('a son kiriting')
+//     let b = +prompt('b son kiriting')
+//     for(let i = a; i <= b; i++){
+//         console.log(i);
+//     }
+// }
+// {
+//     // masala 7
+//     let a = +prompt('a son kiriting')
+//     let b = +prompt('b son kiriting')
+//     for(let i = a; i <= b; i++){
+//         if(i % 2 == 0 && i % 3 == 0){
+//             console.log(i);
+//         }
+//     }
+// }
+
+// {
+//     // masala 8
+//     let a = +prompt('a son kiriting')
+//     for(let i = 1; i <= a; i++){
+//         if( a % i == 0){
+//             console.log(`boluvchilari: ${i}`);
+//         }
+//     }
+// }
+
+// {
+//     // masala 9
+//     // ustoz bu masqlani yecholmadm
+
+
+
+// }
+
+// {
+//     // masala 10
+//     let number = +prompt('ikki xonali son kiriting')
+//     let birlik =Math.floor(number / 10);
+//     let onlik = number % 10;
+//     console.log((birlik + onlik) / onlik +' :katta');
+   
+// }
+
+// {
+//     // masala 11
+//     let array = [true,'25',undefined,null]
+//         for(let i = 0; i < array.length; i++ ){
+//             if(array[i] == true || array[i] == false){
+//                 console.log('bor'); break
+//             }
+//             else{
+//                 console.log('yoq');
+//             }
+//         }
+// }
+
+// {
+//     // masala 12
+//     let array = [1,2,3,4,5,6,7,8,9];
+//     for(let i = 9; i <= array.length;i ){
+//         console.log((array[i]));
+//     }
+// }
+
+// {
+//     // masala 13
+//     let array = [2,4,2]
+//     let sum = 0;
+//     for(let i = 0;i < array.length; i++){
+//        sum += array[i]
+        
+//     }
+//     console.log(`yigindisi: ${sum}`);
+// }
+
+// {
+//     // masala 14
+//     let array = [2,4,true,false,null]
+//         let sum = 0;
+//         for(let i = 0;i < array.length; i++){
+//            sum += array[i]
+            
+//         }
+//         console.log(`yigindisi: ${sum}`);
+
+// }
+
+// {
+//     // masala 15
+//     let array = [-2,3,-4,-3,2]
+//     let sum = 0;
+//     for(let i = 0; i < array.length;i++){
+//         if(array[i] <0){
+//             sum += array[i]
+//         } 
+//     }
+//     console.log(`manfiylar yigindisi: ${sum}`);
+// }
+
+// {
+//     // masala 16
+//     let array = [-2,2,-2,3-3]
+
+// }
+
+// {
+//     // masala 17
+//     for(let i = 20; i >= 1; i--){
+//         console.log(i);
+//     }
+
+
+// }
+
+// {
+//     // masala 18
+//     // ustoz bu masalani ishlomadm
+
+// }
+
+// alohida 5 ta masala 
+// {
+//     // masla 1
+//     let array = [2,-2,3,4];
+//     let sum = 0;
+//     for(let i = 0; i < array.length; i++){
+//         sum += array[i]
+//     }
+//     console.log(sum);
+    
+    
+// }
+
+// {
+//     // masala 2
+//     let number = +prompt('dollar kiriting')
+//     let dollarKursi = 12700;
+//     console.log( number * dollarKursi);
+// }
+
+// {
+//     // masala 3
+//     let number = +prompt('son kiriting')
+//     if(number % 2 == 0){
+//         console.log(number **2 + ' :bu son juft');
+//     }
+//     else{
+//         console.log(number **2 + ' :bu son toq');
+//     }
+
+// }
+
+// {
+//     // masala 4
+//     let number = +prompt('ixtiyoriy son kiriting');
+//     let sum = 0;
+//     for(let i = 1; i <= number; i++){
+//         if(i % 3 ==0){
+//             sum += i
+//         }
+//     }
+//     console.log(`3 ga bolinadiganlar yigindisi: ${sum}`);
+// }
+
+// {
+//     // masala 5
+//     let array = [2,3,4,5];
+//    for(let i = 0; i < array.length; i++){
+//     if(array[i] == 2){
+//         console.log('2 soni ushbu arrayda bor'); break
+//     }
+//     else{
+//         console.log('yoq');
+//     }
+//    }
+
+// }
